fe0706b88c1e75dff991ded2a98cdd24e41ca2cf
naseelemak <taseelemak@gmail.com>
2019-08-02 18:44:04 +0800

    Among many other things, added Post and Profile classes and created the relationships between them the User class.

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class ProfilesController extends Controller
 {
-    //
+    public function index($user)
+    {
+        $user = User::findOrFail($user);
+
+        return view('home', [
+            'user' => $user,
+        ]);
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,15 +7,15 @@
             <img src="https://instagram.fkul8-1.fna.fbcdn.net/vp/e72fb2f1b87d6e9c1fbff4ba55111902/5DE9B4E8/t51.2885-19/s150x150/13597791_261499887553333_1855531912_a.jpg?_nc_ht=instagram.fkul8-1.fna.fbcdn.net" class="rounded-circle">
         </div>
         <div class="col-9 pt-5">
-            <div><h1>natgeo</h1></div>
+            <div><h1>{{ $user->username }}</h1></div>
             <div class="d-flex">
                 <div class="pr-5"><strong>20,709</strong> posts</div>
                 <div class="pr-5"><strong>115m</strong> followers</div>
                 <div class="pr-5"><strong>131</strong> following</div>
             </div>
-            <div class="pt-4 font-weight-bold">National Geographic</div>
-            <div>Experience the world through the eyes of National Geographic photographers.</div>
-            <div><a href="#">natgeo.com</a></div>
+            <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
+            <div>{{ $user->profile->description }}</div>
+            <div><a href="#">{{ $user->profile->url }}</a></div>
         </div>
     </div>
 
