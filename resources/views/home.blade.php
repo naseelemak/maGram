fe699ad2e83061301f4d2dbcb44209a50ae15516
naseelemak <taseelemak@gmail.com>
2019-08-02 13:59:48 +0800

    Add a bunch of changes including login feature and user home page.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,34 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://instagram.fkul8-1.fna.fbcdn.net/vp/e72fb2f1b87d6e9c1fbff4ba55111902/5DE9B4E8/t51.2885-19/s150x150/13597791_261499887553333_1855531912_a.jpg?_nc_ht=instagram.fkul8-1.fna.fbcdn.net" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>natgeo</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>20,709</strong> posts</div>
+                <div class="pr-5"><strong>115m</strong> followers</div>
+                <div class="pr-5"><strong>131</strong> following</div>
             </div>
+            <div class="pt-4 font-weight-bold">National Geographic</div>
+            <div>Experience the world through the eyes of National Geographic photographers.</div>
+            <div><a href="#">natgeo.com</a></div>
         </div>
     </div>
+
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://instagram.fkul8-1.fna.fbcdn.net/vp/167c0028514a408b487d65068d0ba623/5DE3059C/t51.2885-15/sh0.08/e35/s640x640/66713213_178648249839578_8483366547817305007_n.jpg?_nc_ht=instagram.fkul8-1.fna.fbcdn.net" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fkul8-1.fna.fbcdn.net/vp/e6c27c495acf45a3ded8e7cf29d1b2bc/5DCC8EE8/t51.2885-15/sh0.08/e35/s640x640/65301318_886122091765903_4890931908386955984_n.jpg?_nc_ht=instagram.fkul8-1.fna.fbcdn.net" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fkul8-1.fna.fbcdn.net/vp/5c1b0cfb30040c128c28b84e0644a3d0/5DC9CB08/t51.2885-15/sh0.08/e35/c240.0.960.960a/s640x640/64660934_1834856553326868_8048166822925052892_n.jpg?_nc_ht=instagram.fkul8-1.fna.fbcdn.net" class="w-100">
+        </div>
+    </div>
+
 </div>
 @endsection
